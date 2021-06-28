51287074e9ffa6b3769cc143980125296c376d53
Henri Tunkkari <henri.tunkkari@hotmail.com>
2021-06-29 02:15:53 +0300

    Restructure source files

---
+++ opentorsion/__init__.py
@@ -0,0 +1,5 @@
+from opentorsion.disk_element import Disk
+from opentorsion.shaft_element import Shaft
+from opentorsion.gear_element import Gear
+from opentorsion.assembly import Rotor
+from opentorsion.errors import DOF_mismatch_error
