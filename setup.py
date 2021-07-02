123dfa269ca570da4b50ba8ac7048556a8d73e36
Henri Tunkkari <henri.tunkkari@hotmail.com>
2021-07-02 14:44:34 +0300

    Change from setup.cfg to .py and project structure

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="opentorsion",
+    version="0.0.2",
+    author="Aalto ARotor",
+    author_email="todo@aalto.fi",
+    description="Open source library for creating torsional finite element models",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Aalto-Arotor/openTorsion",
+    project_urls={
+        "Bug Tracker": "https://github.com/Aalto-Arotor/openTorsion/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=[
+        "matplotlib",
+        "numpy",
+        "scipy"
+    ]
+)
