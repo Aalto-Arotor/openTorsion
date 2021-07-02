de4009ac01617eed9f1f643fab3f597844fa38a2
Henri Tunkkari <henri.tunkkari@hotmail.com>
2021-07-02 14:48:39 +0300

    Fix imports

--- src/opentorsion/main.py
+++ src/opentorsion/main.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plt
 from scipy import linalg as LA
 from scipy.sparse import linalg as las
 
-from disk_element import Disk
-from shaft_element import Shaft
-from gear_element import Gear
-from assembly import Rotor
+from src.opentorsion.disk_element import Disk
+from src.opentorsion.shaft_element import Shaft
+from src.opentorsion.gear_element import Gear
+from src.opentorsion.assembly import Rotor
 
 assembly = Rotor([Shaft(0,1, 1000, 1)], disk_elements=[Disk(0, 4), Disk(0, 3)])
 
