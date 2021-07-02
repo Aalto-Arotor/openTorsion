27d03df04ae8902493aa4cec21495ae5863ba02c
Henri Tunkkari <henri.tunkkari@hotmail.com>
2021-07-02 14:55:44 +0300

    Bump to v0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="opentorsion",
-    version="0.0.2",
+    version="0.0.3",
     author="Aalto ARotor",
     author_email="todo@aalto.fi",
     description="Open source library for creating torsional finite element models",
