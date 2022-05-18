84d5a87837f3a123b3271c1d9e7b9a656bcc7ab8
Hakonen Urho <hakoneu1@t21204-lr022.org.aalto.fi>
2022-05-18 15:37:42 +0300

    v.0.0.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="opentorsion",
-    version="0.0.6",
+    version="0.0.8",
     author="Aalto ARotor",
     author_email="todo@aalto.fi",
     description="Open source library for creating torsional finite element models",
