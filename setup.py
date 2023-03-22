4f78e7f506e809a81c30942342bc6f0a68ec404f
Hakonen Urho <hakoneu1@t21204-lr022.org.aalto.fi>
2023-03-22 11:45:06 +0200

    fixed error in gear element documentation, removed broken code from assembly

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="opentorsion",
-    version="0.1.2",
+    version="0.1.3",
     author="Aalto ARotor",
     author_email="arotor.software@aalto.fi",
     description="Open source library for creating torsional finite element models",
